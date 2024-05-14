3d9b9280a3f981a83c552f1c60dee9ae41974f22
Gerardo <gerardotoboso1909@gmail.com>
2024-05-14 14:27:49 -0300

    agrego clase Dimension

--- src/App.java
+++ src/App.java
@@ -2,13 +2,26 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        String filePath = "./recursos/productos.csv";
-         List<String[]> datos =  Csv_reader.lector(filePath);
-        for (String[] fields : datos) {
-            for (String field : fields) {
-                System.out.print(field + ", ");
-            }
-            System.out.println(); // Agrega un salto de línea después de imprimir cada línea
+        String filePath1 = "./recursos/ventas.csv";
+        String filePath2 = "./recursos/fechas.csv";
+        String filePath3 = "./recursos/puntos_venta.csv";
+
+        List<String[]> ventas =  Csv_reader.lector(filePath1);
+        List<String[]> fechas = Csv_reader.lector(filePath2);
+        List<String[]> puntos_venta = Csv_reader.lector(filePath3);
+//        for (String[] fields : datos) {
+//            for (String field : fields) {
+//                System.out.print(field + ", ");
+        Dimension dicc_fechas = new Dimension(fechas, ventas);
+        Dimension dicc_puntos_venta = new Dimension(puntos_venta, ventas);
+
+        for (String clave : dicc_puntos_venta.dicc_id.keySet()) {
+            System.out.println(clave);
         }
+//        List<Integer> filas_con_id_fecha_0 = dicc_fechas.dicc_id.get(0);
+//        for( int fila : filas_con_id_fecha_0){
+//            System.out.println(fila);
+//            System.out.println(); // Agrega un salto de línea después de imprimir cada línea
+//        }
     }
 }
---
+++ src/Dimension.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Dimension {
+    Map<String, List<Integer>> dicc_id;
+    
+    public Dimension( List<String[]> df_significados , List<String[]> df_ventas) {
+        Map<String, List<Integer>> dicc_id = new HashMap<>();
+
+        String id_dimension = df_significados.get(0)[1]; // que id es el que va a ser usado
+        int indice_en_df_ventas = -1;
+
+        for(int i = 0; i < df_significados.get(0).length; i++){ // busco en que columna está ese id en df_ventas
+            if (df_significados.get(0)[i].equals(id_dimension)){
+                indice_en_df_ventas = i;
+            }
+        }
+
+        for( String[] fila_ventas : df_ventas){
+            for( String[] fila_significados : df_significados){
+                if( fila_ventas[indice_en_df_ventas].equals(fila_significados[1])){
+                    Agregar.agregarValor(dicc_id, fila_significados[fila_significados.length - 1], Integer.parseInt(fila_ventas[0]));
+                }
+            }
+        }
+
+        this.dicc_id = dicc_id;
+    }
+}
