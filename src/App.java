05c11a9ae2ad53758e73f35901fa06476d5faa61
Marcos Achaval <marcos.achavalr@gmail.com>
2024-05-14 22:40:10 -0300

    saque espacio linea 6

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         String filePath = "./recursos/productos.csv";
-         List<String[]> datos =  Csv_reader.lector(filePath);
+        List<String[]> datos = Csv_reader.lector(filePath);
         for (String[] fields : datos) {
             for (String field : fields) {
                 System.out.print(field + ", ");
